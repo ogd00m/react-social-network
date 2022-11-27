9fafc45144a4aee5f2193781ca7299f7705e5829
ogd00m <crazedstriker1998@gmail.com>
2022-11-27 20:45:51 +0700

    13-lesson

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img
+            src='https://static.wikia.nocookie.net/logopedia/images/0/08/Doomshop_Records_%28Alt%29.png'
+            alt='logo'/>
+
+    </header>
+}
+
+export default Header;
